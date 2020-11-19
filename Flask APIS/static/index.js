8f26547d81333462d59d5fd5f87453d86b91ff6a
Rohit Pai <rohitrpai@gmail.com>
2020-11-19 22:28:33 +0100

    Deleted copy folder of the Flask APIs.

--- Flask APIS/static/index.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    $(".btnPredict").click(function(){
-        $(".predictText").text("Team1")
-    });
-
-    $("#input-image").change(function() {
-        // $("#changedImg").attr('src', '/img/no_preview.png');
-    });
-});
