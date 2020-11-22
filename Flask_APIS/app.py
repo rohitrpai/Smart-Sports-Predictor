a4825e23c1e52401c8f1cf7e3ee69448f8043484
Rohit Pai <rohitrpai@gmail.com>
2020-11-22 11:24:23 +0100

    Updates in API and HTML pages for connection to the DB.

--- Flask_APIS/app.py
+++ Flask_APIS/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors 
 import re
+import yaml
 
 from flask_injector import FlaskInjector
 from injector import inject
@@ -10,14 +11,15 @@ from Flask_APIS.dependencies import configure
   
   
 app = Flask(__name__)
-  
+
+#db = yaml.load(open('db.yaml'))
   
 app.secret_key = 'your secret key'
   
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'dbregistration'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Smart@1010'
+app.config['MYSQL_DB'] = 'predictor'
   
 mysql = MySQL(app)
 
@@ -40,19 +42,20 @@ def main():
 @app.route('/login', methods =['GET', 'POST'])
 def login(service: MyService):
     msg = '' 
-    if request.method == 'POST' and 'Uname' in request.form and 'Pass' in request.form:
-        username = request.form['Uname']
-        password = request.form['Pass']
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
         if not username or not password:
             msg = 'Please enter your username/password!'
             return render_template('login.html', msg=msg)
         #cursor = service.get_db_cursor()
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, )) 
+        cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", [username, password])
         account = cursor.fetchone()
+        print(account)
         if account: 
             session['loggedin'] = True
-            session['id'] = account['id'] 
+            #session['id'] = account['id']
             session['username'] = account['username'] 
             msg = 'Logged in successfully !'
             return redirect(url_for("home"))
@@ -75,28 +78,29 @@ def logout():
 @app.route('/register', methods =['GET', 'POST']) 
 def register(service: MyService):
     msg = '' 
-    if request.method == 'POST' and 'Uname' in request.form and 'Pass' in request.form and 'Email_id' in request.form :
-        username = request.form['Uname']
-        password = request.form['Pass']
-        email = request.form['Email_id']
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
         #cursor = service.get_db_cursor()
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, )) 
-        account = cursor.fetchone() 
-        if account: 
+        cursor.execute("SELECT * FROM users WHERE username = %s", [username])
+        account = cursor.fetchone()
+        if account:
             msg = 'Account already exists !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email): 
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username): 
+        elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email: 
+        elif not username or not password or not email:
             msg = 'Please fill out the form !'
-        else: 
-            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+        else:
+            cursor.execute("INSERT INTO users(username,password,email_id) VALUES (% s, % s, % s)", [username, password, email])
             #service.get_sql_instance().connection.commit()
             mysql.connection.commit()
+            cursor.close()
             msg = 'You have successfully registered !'
-    elif request.method == 'POST': 
+    elif request.method == 'POST':
         msg = 'Please fill out the form !'
     return render_template('registration.html', msg = msg)
 
@@ -109,7 +113,7 @@ def home():
         if uname == "Unknown":
             return render_template('login.html', msg='You need to sign in!')
         else:
-            return render_template('index.html', msg)
+            return render_template('index.html')
     '''elif request.method == 'POST' or request.method == 'GET':
         msg = 'Failed'
         return msg'''
