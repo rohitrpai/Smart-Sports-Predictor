64c9864bf260ca961c9be039a56ab89792405b98
raufhuseynov <41780609+raufhuseynov@users.noreply.github.com>
2020-11-28 14:01:47 +0400

    Update index.js

--- Flask_APIS/static/index.js
+++ Flask_APIS/static/index.js
@@ -1,9 +1,15 @@
-$(document).ready(function () {
-    $(".btnPredict").click(function () {
+$( document ).ready(function() {
+    $(".btnPredict").click(function(){
         $(".predictText").text("Sunrisers Hyderabad")
     });
 
-    $("#input-image").change(function () {
+    $("#input-image").change(function() {
         // $("#changedImg").attr('src', '/img/no_preview.png');
+    });
+
+    $('select').change(function() {
+        var selectedOption = $(this).find("option:selected").text()
+        $(this).siblings('select').find('option').attr('disabled', false);
+        $(this).siblings('select').find('option:contains(' + selectedOption + ')').attr('disabled',true);
     });
 });
